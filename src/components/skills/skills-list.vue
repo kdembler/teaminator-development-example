7689e2974793c85fdea3e3d35bd29cc0e10a04b9
Klaudiusz Dembler <k.dembler@gmail.com>
2018-08-24 18:20:44 +0200

    Add avatars in skills list

--- src/components/skills/skills-list.vue
+++ src/components/skills/skills-list.vue
@@ -3,7 +3,7 @@
     <b-list-group-item>{{ header }}</b-list-group-item>
     <template v-for="skill in skills">
       <b-list-group-item
-        v-bind:class="{'bg-dark': opened[skill.id], 'text-white': opened[skill.id]}"
+        v-bind:class="{'bg-dark': opened[skill.id], 'text-white': opened[skill.id], 'skill-expanded': opened[skill.id]}"
         v-b-toggle="`${skill.id}-accordion`"
         :key="`${skill.id}-header`"
       >
@@ -27,6 +27,7 @@
         <b-list-group-item v-for="(value, user) in skill.users" :key="`${skill.id}-${user}`">
           <b-row align-h="between">
             <b-col cols="10">
+              <avatar :userId="user" :url="team[user].avatar" size="40" class="mr-2" />
               {{ team[user].name }}
             </b-col>
             <b-col cols="2">
@@ -40,13 +41,17 @@
 </template>
 
 <script>
+import Avatar from '../avatar';
 import ColoredProgressCircle from '../progress/coloredProgressCircle';
 
 export default {
   name: 'SkillsList',
+
   components: {
+    Avatar,
     ColoredProgressCircle
   },
+
   props: {
     header: {
       type: String,
@@ -62,6 +67,7 @@ export default {
       required: true
     }
   },
+
   data() {
     return {
       opened: {}
@@ -110,14 +116,14 @@ $divider-color: #e9eff2;
     border-bottom: 1px solid $divider-color;
   }
 
-  &:not(:first-child):not(:nth-last-child(2)):not(.bg-dark)::after {
+  &:not(:first-child):not(:nth-last-child(2)):not(.skill-expanded)::after {
     @extend %divider;
   }
 }
 
 .users-list > .list-group-item {
   background-color: white;
-  padding: 0.75rem 1.6rem;
+  padding: 0.75rem 2rem;
 
   &:not(:last-child)::after {
     @extend %divider;
--- src/components/skills/skills.vue
+++ src/components/skills/skills.vue
@@ -19,8 +19,6 @@
 </template>
 
 <script>
-// Color circle to present progress
-import ColoredProgressCircle from '../progress/coloredProgressCircle';
 import SkillsList from './skills-list';
 
 function parseSkills(skills, team) {
@@ -61,28 +59,14 @@ function parseTeam(team) {
 }
 
 export default {
-  // Name of component for registration
   name: 'Skills',
 
-  // Register components with the vue template
   components: {
-    ColoredProgressCircle,
     SkillsList
   },
 
-  // list of injected properties, since this component Skills to a top level view referenced in routes.js, it likely won't need any, child components will have these
-  props: {
-    propertyName: {
-      type: Object,
-      required: false,
-      default() { return {}; }
-    }
-  },
-
-  // application level classes which handle most of the heavy lifting, these are standard javascript classes.
   inject: ['logger', 'alertHandler', 'skillsClient', 'teamIdentityClient'],
 
-  // mutable data for the component
   data() {
     return {
       isLoading: true,
@@ -92,7 +76,6 @@ export default {
     };
   },
 
-  // code executed only when the component is created, but once for each instance
   async created() {
     let skills = await this.skillsClient.getTeamSkillReport();
     let team = await this.teamIdentityClient.getTeamMembers();
@@ -105,7 +88,6 @@ export default {
     this.isLoading = false;
   },
 
-  // Reactive properties which will update when the properties used inside these also update
   computed: {
     formattedDate() {
       const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dev'];
@@ -116,10 +98,6 @@ export default {
 
       return `${month} ${day}, ${year}`;
     }
-  },
-
-  // Defines a list of methods which can be called
-  methods: {
   }
 };
 
